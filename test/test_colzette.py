36ade54c01fda9985b608e9f4f52c8ae216f6dd5
baugetfa <fabrice.bauget@cirad.fr>
2026-03-24 11:46:52 +0100

    simple test_colzette just to have one to start

--- test/test_colzette.py
+++ test/test_colzette.py
@@ -1,3 +1,8 @@
 import pytest
 
-from openalea.colzette.colzette_lib import *
+from openalea.colzette.colzette_lib import get_nb_leaflets
+
+def test_get_nb_leaflets():
+
+    n = get_nb_leaflets(10)
+    assert n == 5
