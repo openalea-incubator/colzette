9b2b6c54a2e4b5120a1453573ffd339ab6f69bcb
baugetfa <fabrice.bauget@cirad.fr>
2026-04-02 10:17:22 +0200

    [no ci] WIP clean duplicated functions or class generate population

--- src/openalea/colzette/population.py
+++ src/openalea/colzette/population.py
@@ -46,6 +46,25 @@ def generate_fababean_population(sowing_pattern,
         list_of_positions.append((xi, yi, 0))
     return list_of_MTGs, list_of_positions
 
+def generate_population(sowing_pattern,
+                        dict_params,
+                        vec_TLA,
+                        PlantAge,
+                        iday,
+                        vegetative,
+                        phenotype):
+    list_of_MTGs = []
+    list_of_positions = []
+    nplants = len(sowing_pattern)
+    for id in range(0, nplants):
+        xi = sowing_pattern['x'][id] * 100
+        yi = sowing_pattern['y'][id] * 100
+        gi = vegetative(DJ=PlantAge, dict_params=dict_params, coord=[(xi, yi, 0)])
+        phenotype(gi, total_surface=vec_TLA[iday], dict_params=dict_params)
+
+        list_of_MTGs.append(gi.copy())
+        list_of_positions.append((xi, yi, 0))
+    return list_of_MTGs, list_of_positions
 
 def generate_mixture_population(sowing_pattern,
                                 dict_params_rape,
