bb42719121505266795d93d708cc9104b52ad9b7
baugetfa <fabrice.bauget@cirad.fr>
2026-04-02 16:20:16 +0200

    [no ci] WIP single functions for population and new function to run static simulation

--- src/openalea/colzette/population.py
+++ src/openalea/colzette/population.py
@@ -1,7 +1,8 @@
 
 from openalea.colzette.geometry import (
     vegetative_fababean, phenotype_fababean,
-    vegetative_rapeseed, phenotype_rapeseed)
+    vegetative_rapeseed, phenotype_rapeseed,
+    vegetative)
 
 def generate_rapeseed_population(sowing_pattern, dict_params_rape, vec_TLA_rape, PlantAge_rape, iday):
     list_of_MTGs = []
@@ -12,7 +13,7 @@ def generate_rapeseed_population(sowing_pattern, dict_params_rape, vec_TLA_rape,
         xi = sowing_pattern['x'][id] * 100
         yi = sowing_pattern['y'][id] * 100
         gi = vegetative_rapeseed(DJ=PlantAge_rape,
-                                 dict_params_rape=dict_params_rape,
+                                 dict_params=dict_params_rape,
                                  coord=[(xi, yi, 0)])
         phenotype_rapeseed(gi,
                            total_surface=vec_TLA_rape[iday],
@@ -21,7 +22,6 @@ def generate_rapeseed_population(sowing_pattern, dict_params_rape, vec_TLA_rape,
         list_of_MTGs.append(gi.copy())
         list_of_positions.append((xi, yi, 0))
     return list_of_MTGs, list_of_positions
-
 
 def generate_fababean_population(sowing_pattern,
                                  dict_params_faba,
@@ -36,7 +36,7 @@ def generate_fababean_population(sowing_pattern,
         xi = sowing_pattern['x'][id] * 100
         yi = sowing_pattern['y'][id] * 100
         gi = vegetative_fababean(DJ=PlantAge_faba,
-                                 dict_params_faba=dict_params_faba,
+                                 dict_params=dict_params_faba,
                                  coord=[(xi, yi, 0)])
         phenotype_fababean(gi,
                            total_surface=vec_TLA_faba[iday],
@@ -48,19 +48,18 @@ def generate_fababean_population(sowing_pattern,
 
 def generate_population(sowing_pattern,
                         dict_params,
-                        vec_TLA,
+                        TLA,
                         PlantAge,
-                        iday,
-                        vegetative,
-                        phenotype):
+                        phenotype,
+                        species='Rapeseed'):
     list_of_MTGs = []
     list_of_positions = []
     nplants = len(sowing_pattern)
     for id in range(0, nplants):
         xi = sowing_pattern['x'][id] * 100
         yi = sowing_pattern['y'][id] * 100
-        gi = vegetative(DJ=PlantAge, dict_params=dict_params, coord=[(xi, yi, 0)])
-        phenotype(gi, total_surface=vec_TLA[iday], dict_params=dict_params)
+        gi = vegetative(DJ=PlantAge, dict_params=dict_params, coord=[(xi, yi, 0)], species=species)
+        phenotype(gi, total_surface=TLA, dict_params=dict_params)
 
         list_of_MTGs.append(gi.copy())
         list_of_positions.append((xi, yi, 0))
